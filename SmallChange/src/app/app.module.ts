66bf1afa0d31f2f906e11c56680da83785f670cb
Rahul Kaushik <rahul.kaushik1806@gmail.com>
2022-09-06 21:00:21 +0530

    App Module import fixes

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -3,29 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginpageComponent } from './loginpage/loginpage.component';
+import { LoginpageModule } from './loginpage/loginpage.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CoreModule } from './core/core.module';
 import { PreferencesModule } from './preferences/preferences.module';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { PortfolioTableComponent } from './portfolio-table/portfolio-table.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginpageComponent,
-    PortfolioComponent,
-    PortfolioTableComponent
-  ],
+  declarations: [AppComponent, PortfolioComponent, PortfolioTableComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    CoreModule,
-    PreferencesModule
-    LoginpageModule
+    PreferencesModule,
+    LoginpageModule,
+    CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
