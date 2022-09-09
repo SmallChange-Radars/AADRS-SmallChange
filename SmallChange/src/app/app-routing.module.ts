a30c520da4b9eb976483db9c10993343bb06681d
DhivakarK-git <dhivakar189@gmail.com>
2022-09-09 09:45:33 +0530

    Header + NavBar Change

--- SmallChange/src/app/app-routing.module.ts
+++ SmallChange/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { PortfolioPageComponent } from './portfolio/portfolio-page/portfolio-page.component';
+import { PreferencesPageComponent } from './preferences/preferences-page/preferences-page.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginpageComponent
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent
+  },
+  {
+    path: 'preferences',
+    component: PreferencesPageComponent
+  },
+  {
+    path: 'trade',
+    component: LoginpageComponent
+  },
+  {
+    path: 'activity',
+    component: RegisterFormComponent
+  },
+  {
+    path: '',
+    component: PortfolioPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
