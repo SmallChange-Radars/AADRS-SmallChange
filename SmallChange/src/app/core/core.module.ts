502e713bc052e948e13ec5ec22188d0837dd6eea
Rahul Kaushik <rahul.kaushik1806@gmail.com>
2022-09-28 09:08:59 +0530

    Initial Navbar Movement

--- SmallChange/src/app/core/core.module.ts
+++ SmallChange/src/app/core/core.module.ts
@@ -3,22 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
-
-
+import { AppRoutingModule } from '../app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    NavBarComponent
-  ],
-  imports: [
-    CommonModule,    
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    NavBarComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent, NavBarComponent],
+  imports: [CommonModule, AppRoutingModule, NgbModule],
+  exports: [HeaderComponent, FooterComponent, NavBarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
--- SmallChange/src/app/core/nav-bar/nav-bar/nav-bar.component.ts
+++ SmallChange/src/app/core/nav-bar/nav-bar/nav-bar.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  navbarOpen = false;
+  public clicked = false;
+  _el: any;
+  constructor(private user: UserService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
+  logOut() {
+    this.user.removeUser();
+    this.router.navigate(['/']);
+  }
+
+  login() {
+    this.router.navigate(['/login']);
+  }
+
+  register() {
+    this.router.navigate(['/register']);
+  }
+
+  isLoggedIn() {
+    return this.user.isLoggedIn();
+  }
+
+  prefer() {
+    this.user.removeUser();
+    this.router.navigate(['/preferences']);
+  }
 }
