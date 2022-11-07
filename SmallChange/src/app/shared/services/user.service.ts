4282d28805b6bfe727669c2522483b585739e177
DhivakarK-git <dhivakar189@gmail.com>
2022-11-07 13:52:15 +0530

    login stuff

--- SmallChange/src/app/shared/services/user.service.ts
+++ SmallChange/src/app/shared/services/user.service.ts
@@ -3,30 +3,49 @@ import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { StateService } from './state.service';
 
+interface accessToken {
+  id: string[]
+}
+
+const initialState: accessToken = {
+  id: []
+};
+
+
 @Injectable({
   providedIn: 'root'
 })
-export class UserService{
+export class UserService extends StateService<accessToken>{
 
-  
+  accessToken$: Observable<string[]> = this.select(state => state.id);
 
   constructor(private cookieService: CookieService) {
-    
+    super(initialState);
   }
 
   addUser(token: string) {
-    this.cookieService.set("accessToken",token);
+    this.setState({ id: [token] })
+    this.cookieService.set("accessToken", token);
   }
 
   removeUser() {
+    this.setState({ id: [] })
     this.cookieService.delete("accessToken");
   }
 
   getUser() {
-    return this.cookieService.get("accessToken");
+    if (this.cookieService.get("accessToken")) {
+      console.log("Yes");
+      this.setState({ id: [this.cookieService.get("accessToken")] })
+    }
+    else {
+      this.setState({ id: [] })
+    }
+    return this.state.id[0];
   }
 
   isLoggedIn(): boolean {
-    return this.getUser != null;
+    this.getUser();
+    return this.state.id.length != 0;
   }
 }
