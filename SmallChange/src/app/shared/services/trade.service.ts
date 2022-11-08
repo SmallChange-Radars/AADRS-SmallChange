40ed1ffdaa1ffe6bcd54aa7ccff391571c11f089
DhivakarK-git <dhivakar189@gmail.com>
2022-11-08 09:35:37 +0530

    removed unused code

--- SmallChange/src/app/shared/services/trade.service.ts
+++ SmallChange/src/app/shared/services/trade.service.ts
@@ -27,24 +27,4 @@ export class TradeService {
     }
     return this.http.get<Instrument[]>(url, { headers: headers, observe: "response" });
   }
-  
-  // getStocks(pageNo: number, pageSize: number, query: string): Observable<HttpResponse<Trade[]>> {
-  //   let url = this.url + '?q=' + query + '&_page=' + pageNo + '&_limit=' + pageSize;
-  //   return this.http.get<Trade[]>(url, { observe: "response" });
-  // }
-
-  // getStocks(pageNo: number, pageSize: number): Observable<HttpResponse<Trade[]>> {
-  //   let url = this.url + '?_page=' + pageNo + '&_limit=' + pageSize;
-  //   return this.http.get<Trade[]>(url, {observe: "response"});
-  // }
-
-  // getSearchStocksSymbol(searchText: string) {
-  //   let url = this.url + '?Symbol_like=.*' + searchText + '.*';
-  //   return this.http.get<Trade[]>(url);
-  // }
-
-  // getSearchStocksName(searchText: string) {
-  //   let url = this.url + '?Name_like=.*' + searchText + '.*';
-  //   return this.http.get<Trade[]>(url);
-  // }
 }
