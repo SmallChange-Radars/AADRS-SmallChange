f6f5a0bf21a0a1aa06849a2f8bc5f7e48e3436f4
shreeshyadav <shreeshyadav@gmail.com>
2022-11-09 12:49:45 +0530

    trade activity with filter and search completed

--- SmallChange/src/app/shared/models/activity.ts
+++ SmallChange/src/app/shared/models/activity.ts
@@ -3,10 +3,10 @@ import { Stock } from "./stock";
 export class Activity {
     constructor(
         public id: string,
-        public Stock: string,
-        public Quantity: number,
-        public Price: number,
-        public Type: string,
-        public Date: Date
+        public instrumentId: string,
+        public quantity: number,
+        public cashValue: number,
+        public direction: string,
+        public timestamp: string
     ){}
 }
